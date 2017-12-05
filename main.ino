aad0ca31860c39cdf180cd6317aa125ec03af2a0
Michał Kutrzeba <kutrzeba.michal@gmail.com>
2017-12-05 23:28:58 +0100

    change rtos to millis

--- main.ino
+++ main.ino
@@ -12,12 +12,14 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 // Heating program
 static uint8_t heatingProgram = 1;
-// DHT11
+// Sensors
+#define SENSOR_READ_INTERVAL 3000 // couldn't be less than 2s
 static uint8_t temperature;
 static uint8_t humidity;
-static uint32_t timer = -9999;
+static uint32_t sensorReadTim = -9999;
 // LCD
-#define LCD_LIGHT_MAX_TIM 300
+#define LCD_LIGHT_MAX_TIM 5000
+static uint8_t backlighState = 0;
 static uint8_t lcdState = 0;
 static uint8_t lcdLight = 0;
 static uint32_t lcdLightTim = LCD_LIGHT_MAX_TIM;
@@ -33,10 +35,23 @@ static char zn;
 static char txt[150];
 static uint32_t index;
 #define FUNC_LOAD_PROGRAM 1
+
+#define BT_COMMAND_LISTEN 255
+
+#define BT_STATE_MAIN         0
+#define BT_STATE_HEAT_PROGRAM 1
 // Menu
-#define MENU_BLINK_TIM 15
-#define MENU_CHANGE_PROGRAM_TIM_THRESHOLD 500
-static uint8_t menuBlinkTimStatus = 0;
+#define MENU_BLINK_TIM 500
+#define MENU_CHANGE_PROGRAM_TIM_THRESHOLD 2000
+
+#define MENU_STATE_MAIN              0
+#define MENU_STATE_LIMITS            1
+#define MENU_STATE_PROGRAM           2
+#define MENU_STATE_SET_PROGRAM_ENTER 3
+#define MENU_STATE_SET_PROGRAM       4
+
+static uint32_t menuBlinkTimStatus;
+static uint32_t menuBlinkTimDiff;
 static uint32_t settingProgramCounter = 0;
 
 void setup ()
@@ -53,59 +68,73 @@ void setup ()
 
 void menu()
 {
+    if (btn1 || btn2) {
+        lcdLightTim = millis();
+    }
+
+    if (millis() - lcdLightTim < LCD_LIGHT_MAX_TIM) {
+        if (1 != backlighState) {
+            lcd.backlight();
+            backlighState = 1;
+            return;
+        }
+    } else {
+        if (2 != backlighState) {
+            lcd.noBacklight();
+            backlighState = 2;
+            lcdState = MENU_STATE_MAIN;
+        }
+
+        // No action when menu is off.
+        return;
+    }
+
     switch (lcdState) {
-        // Status page.
         default:
-        case 0:
+        case MENU_STATE_MAIN:
+            menuBlinkTimDiff = millis() - menuBlinkTimStatus;
+
             lcd.setCursor(0, 0);
             lcd.print("Temp      ");
             lcd.print(temperature);
-            lcd.print("*C ");
-            lcd.print(menuBlinkTimStatus > MENU_BLINK_TIM ? "_" : " ");
+            lcd.print((char) 223);
+            lcd.print("C ");
+            lcd.print(menuBlinkTimDiff > MENU_BLINK_TIM ? "_" : " ");
             lcd.setCursor(0, 1);
             lcd.print("Humid     ");
             lcd.print(humidity);
             lcd.print("%  ");
-            lcd.print(menuBlinkTimStatus > MENU_BLINK_TIM ? "_" : " ");
+            lcd.print(menuBlinkTimDiff > MENU_BLINK_TIM ? "_" : " ");
 
-            menuBlinkTimStatus++;
-            if (menuBlinkTimStatus > 2 * MENU_BLINK_TIM) {
-                menuBlinkTimStatus = 0;
+            if (menuBlinkTimDiff > 2 * MENU_BLINK_TIM) {
+                menuBlinkTimStatus = millis();
             }
 
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && btn1 && !pbtn1) {
-                lcdState = 2;
+            if (btn1 && !pbtn1) {
+                lcdState = MENU_STATE_PROGRAM;
             }
 
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && btn2) {
-                lcdState = 1;
-            }
-
-            if (lcdLightTim > LCD_LIGHT_MAX_TIM) {
-                lcdState = 0;
+            if (btn2) {
+                lcdState = MENU_STATE_LIMITS;
             }
 
             break;
-        case 1:
-            // Settings
+        case MENU_STATE_LIMITS:
             lcd.setCursor(0, 0);
-            lcd.print("Min temp     ");
-            lcd.print("22*C     ");
+            lcd.print("Min temp   ");
+            lcd.print("22");
+            lcd.print((char) 223);
+            lcd.print("C     ");
             lcd.setCursor(0, 1);
-            lcd.print("Min humid    ");
+            lcd.print("Min humid  ");
             lcd.print("65%      ");
 
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && !btn2) {
-                lcdState = 0;
-            }
-
-            if (lcdLightTim > LCD_LIGHT_MAX_TIM) {
-                lcdState = 0;
+            if (!btn2) {
+                lcdState = MENU_STATE_MAIN;
             }
 
             break;
-        case 2:
-            // Show program
+        case MENU_STATE_PROGRAM:
             lcd.setCursor(0, 0);
             lcd.print("Program         ");
             lcd.setCursor(0, 1);
@@ -113,48 +142,41 @@ void menu()
             lcd.print(heatingProgram);
             lcd.print(" | Test       ");
 
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && btn1 && !pbtn1) {
-                lcdState = 0;
+            if (btn1 && !pbtn1) {
+                lcdState = MENU_STATE_MAIN;
             }
 
             // Hold btn2 to enter change program mode.
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && btn2 && !pbtn2) {
-                lcdState = 3;
-            }
-
-            if (lcdLightTim > LCD_LIGHT_MAX_TIM) {
-                lcdState = 0;
+            if (btn2 && !pbtn2) {
+                settingProgramCounter = millis();
+                lcdState = MENU_STATE_SET_PROGRAM_ENTER;
             }
 
             break;
-        case 3:
+        case MENU_STATE_SET_PROGRAM_ENTER:
             if (!btn2) {
-                settingProgramCounter = 0;
-                lcdState = 2;
+                lcdState = MENU_STATE_PROGRAM;
             }
 
-            if (settingProgramCounter > MENU_CHANGE_PROGRAM_TIM_THRESHOLD) {
-                settingProgramCounter = 0;
-                lcdState = 4;
+            if (millis() - settingProgramCounter > MENU_CHANGE_PROGRAM_TIM_THRESHOLD) {
+                lcdState = MENU_STATE_SET_PROGRAM;
             }
 
-            settingProgramCounter++;
             break;
-        case 4:
-            // Change program
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && btn2 && !pbtn2) {
+        case MENU_STATE_SET_PROGRAM:
+            if (btn2 && !pbtn2) {
                 heatingProgram++;
                 if (heatingProgram > 5) {
                     heatingProgram = 1;
                 }
             }
 
-            menuBlinkTimStatus++;
-            if (menuBlinkTimStatus > 2 * MENU_BLINK_TIM) {
-                menuBlinkTimStatus = 0;
+            menuBlinkTimDiff = millis() - menuBlinkTimStatus;
+            if (menuBlinkTimDiff > 2 * MENU_BLINK_TIM) {
+                menuBlinkTimStatus = millis();
             }
 
-            if (menuBlinkTimStatus > MENU_BLINK_TIM) {
+            if (menuBlinkTimDiff > MENU_BLINK_TIM) {
                 lcd.setCursor(0, 0);
                 lcd.print("Program         ");
                 lcd.setCursor(0, 1);
@@ -168,32 +190,21 @@ void menu()
                 lcd.print("                ");
             }
 
-            if (lcdLightTim < LCD_LIGHT_MAX_TIM && btn1 && !pbtn1) {
-                lcdState = 2;
+            if (btn1 && !pbtn1) {
+                lcdState = MENU_STATE_PROGRAM;
             }
 
-            if (lcdLightTim > LCD_LIGHT_MAX_TIM) {
-                lcdState = 0;
-            }
-
+            break;
+        case MENU_STATE_BLUETOOTH_CONNECTION:
+            lcd.setCursor(0, 0);
+            lcd.print("Bluetooth       ");
+            lcd.setCursor(0, 1);
+            lcd.print("ON              ");
             break;
     }
 }
 
-void lcdLightControl()
-{
-    if (lcdLightTim < LCD_LIGHT_MAX_TIM) {
-        lcdLightTim++;
-        lcd.backlight();
-    } else {
-        lcd.noBacklight();
-    }
-
-    if (btn1 || btn2) {
-        lcdLightTim = 0;
-    }
-}
-
+static uint8_t btState = 0;
 void bluetoothRead()
 {
     if (!Serial.available()) {
@@ -201,36 +212,52 @@ void bluetoothRead()
     }
 
     zn = Serial.read();
-    if (zn == '#') {
-        bluetoothClearBuff();
-        return;
-    } else if (1 == index) {
-        txt[index++] = zn;
-        txt[index] = '\0';
-    } else
+    switch (btState) {
+        default:
+        case BT_STATE_MAIN:
+            switch (zn) {
+                case BT_STATE_HEAT_PROGRAM:
+                    btState = BT_STATE_HEAT_PROGRAM;
+                return;
+            }
+
+            break;
+        case BT_STATE_HEAT_PROGRAM:
+            if (zn == BT_COMMAND_LISTEN) {
+                btState = BT_STATE_MAIN;
+                return;
+            }
+
+            if (1 == index) {
+                txt[index++] = zn;
+                txt[index] = '\0';
+            }
+
+            break;
+    }
 
     if (strcmp(txt, "t1") == 0) {
         //
     }
 }
 
-void bluetoothClearBuff() {
-    memset(txt, '\0', 10);
-    index = 0;
+void readSensors()
+{
+    if ((millis() - sensorReadTim) >= SENSOR_READ_INTERVAL) {
+        humidity = dht.readHumidity();
+        temperature = dht.readTemperature();
+        sensorReadTim = millis();
+    }
 }
 
 void loop()
 {
     btn1 = digitalRead(PIN_BTN1);
     btn2 = digitalRead(PIN_BTN2);
-    menu();
-    lcdLightControl();
 
-    if ((millis() - timer) >= 3000) {
-        humidity = dht.readHumidity();
-        temperature = dht.readTemperature();
-        timer = millis();
-    }
+    menu();
+    readSensors();
+
     pbtn1 = btn1;
     pbtn2 = btn2;
 }
